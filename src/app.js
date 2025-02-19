6c1432d348e6001196af124a90bebb2ba4999f94
Ritik220519 <ritik220519@gmail.com>
2025-02-19 15:21:41 +0530

    http-Routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
-
-
+app.get("/user", (req , res)=>{
+    res.send({name: "Ritik" , lastName : "srivastava"})
+})
+app.post("/user", (req , res)=>{
+    // saving data to the database
+    res.send("Data has been saved")
+})
+app.delete("/user", (req , res)=>{
+    
+    res.send("Data deleted successfully")
+})
 app.use("/hello" , (req , res)=>{
     res.send("hello , hello , hello")
 });
@@ -12,6 +21,9 @@ app.use("/test" , (req , res)=>{
 app.use("/namaste" , (req , res)=>{
     res.send("Namaste dosto")
 });
+// app.use("/" , (req , res)=>{
+//     res.send("hello from the server side")
+// });
 
 app.listen(3000 , ()=>{
     console.log("server is successfully running on port 3000");
